178bd6a54dfb6abd81c78560c4946f0f61ff3c72
Jamie Gobeille <djent34@gmail.com>
2020-07-10 16:05:19 +0900

    created signup page

---
+++ client/src/components/UserSignUp.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+/**
+ * Renders a signUp Page
+ */
+
+export default class UserSignUp extends Component {
+  state = {
+    name: '',
+    username: '',
+    password: '',
+    errors: [],
+  };
+
+  render() {
+    const { name, username, password, errors } = this.state;
+
+    return (
+      <div className="bounds">
+        <div className="grid-33 centered signin">
+          <h1>Sign Up</h1>
+          <Form
+            cancel={this.cancel}
+            errors={errors}
+            submit={this.submit}
+            submitButtonText="Sign Up"
+            elements={() => (
+              <React.Fragment>
+                <input id="name" name="name" type="text" value={name} onChange={this.change} placeholder="Name" />
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={username}
+                  onChange={this.change}
+                  placeholder="User Name"
+                />
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={password}
+                  onChange={this.change}
+                  placeholder="Password"
+                />
+              </React.Fragment>
+            )}
+          />
+          <p>
+            Already have a user account? <Link to="/signin">Click here</Link> to sign in!
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
