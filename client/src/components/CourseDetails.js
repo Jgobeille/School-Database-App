9c66f32d98ff191d6db47b97d2275ede9a304ab2
Jamie Gobeille <djent34@gmail.com>
2020-07-16 21:15:08 +0900

    deleted course actions details bar because not needed anymore

--- client/src/components/CourseDetails.js
+++ client/src/components/CourseDetails.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-
-import CourseDetailsActionsBar from './CourseDetailsActionsBar.js';
+import { Link } from 'react-router-dom';
 
 /**
  * Renders the description and details of a course
@@ -26,11 +25,12 @@ export default class CourseDetails extends Component {
   render() {
     const { courseDetails } = this.state;
     // eslint-disable-next-line react/prop-types
-    const { match } = this.props;
+    const { match, context } = this.props;
 
     const updateURL = `${match.url}/update`;
 
     const { user } = courseDetails;
+
     let materials;
 
     if (courseDetails.materialsNeeded) {
@@ -40,41 +40,71 @@ export default class CourseDetails extends Component {
     // eslint-disable-next-line react/destructuring-assignment
     // eslint-disable-next-line react/prop-types;
     return (
-      <React.Fragment>
-        <CourseDetailsActionsBar url={updateURL} />
-        <div className="bounds course--detail">
-          <div className="grid-66">
-            <div className="course--header">
-              <h4 className="course--label">Course</h4>
-              <h3 className="course--title">{courseDetails.title}</h3>
-              <p>By: {user ? `${user.firstName} ${user.lastName}` : ''}</p>
+      <div>
+        {courseDetails ? (
+          <React.Fragment>
+            <div className="actions--bar">
+              <div className="bounds">
+                <div className="grid-100">
+                  {context.authenticatedUser.id === courseDetails.userId ? (
+                    <span>
+                      <Link className="button" to={updateURL}>
+                        Update Course
+                      </Link>
+                      <Link className="button" to="/">
+                        Delete Course
+                      </Link>
+                      <Link className="button button-secondary" to="/">
+                        Return to List
+                      </Link>
+                    </span>
+                  ) : (
+                    <Link className="button button-secondary" to="/">
+                      Return to List
+                    </Link>
+                  )}
+                </div>
+              </div>
             </div>
-            <div className="course--description">
-              <p>{courseDetails.description}</p>
-            </div>
-          </div>
-          <div className="grid-25 grid-right">
-            <div className="course--stats">
-              <ul className="course--stats--list">
-                <li className="course--stats--list--item">
-                  <h4>Estimated Time</h4>
-                  <h3>{courseDetails.estimatedTime}</h3>
-                </li>
-                <li className="course--stats--list--item">
-                  <h4>Materials Needed</h4>
-                  <ul>
-                    {materials
-                      ? materials
-                          .slice(1)
-                          .map((material, id) => <li key={id}>{material}</li>)
-                      : ''}
+            <div className="bounds course--detail">
+              <div className="grid-66">
+                <div className="course--header">
+                  <h4 className="course--label">Course</h4>
+                  <h3 className="course--title">{courseDetails.title}</h3>
+                  <p>By: {user ? `${user.firstName} ${user.lastName}` : ''}</p>
+                </div>
+                <div className="course--description">
+                  <p>{courseDetails.description}</p>
+                </div>
+              </div>
+              <div className="grid-25 grid-right">
+                <div className="course--stats">
+                  <ul className="course--stats--list">
+                    <li className="course--stats--list--item">
+                      <h4>Estimated Time</h4>
+                      <h3>{courseDetails.estimatedTime}</h3>
+                    </li>
+                    <li className="course--stats--list--item">
+                      <h4>Materials Needed</h4>
+                      <ul>
+                        {materials
+                          ? materials
+                              .slice(1)
+                              .map((material, id) => (
+                                <li key={id}>{material}</li>
+                              ))
+                          : ''}
+                      </ul>
+                    </li>
                   </ul>
-                </li>
-              </ul>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </React.Fragment>
+          </React.Fragment>
+        ) : (
+          <div>Waiting...</div>
+        )}
+      </div>
     );
   }
 }
